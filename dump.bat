995e475375e4927bfeeebee42e53b61de78aa9e4
Mansuang Pawong <mansaung@hotmail.com>
2016-09-19 11:21:12 +0700

    check if dump error then terminate

--- dump.bat
+++ dump.bat
@@ -18,23 +18,26 @@ call config.cmd
  set tableName=%local_database_prefix%_%yy%%mon%%dd%%hh%%min%
 
 echo STEP 1 of 6 :dumping database to %tableName%.sql. Please Wait.
-mysqldump -h %remote_host% -u %remote_user% -p"%remote_password%" %remote_database% > %tableName%.sql
+mysqldump -h %remote_host% -u x%remote_user% -p"%remote_password%" %remote_database% > %tableName%.sql && (
 
-echo STEP 2 of 6 :create backup database.
-mysql -uroot -e"CREATE DATABASE %tableName%"
+	echo STEP 2 of 6 :create backup database.
+	mysql -uroot -e"CREATE DATABASE %tableName%"
 
-echo STEP 3 of 6 :backing up database.
-mysqldump -u %local_user% %local_database_prefix% | mysql -uroot %tableName%
+	echo STEP 3 of 6 :backing up database.
+	mysqldump -u %local_user% %local_database_prefix% | mysql -uroot %tableName%
 
-echo STEP 4 of 6 :drop database %local_database_prefix%.
-mysql -u %local_user% -e"DROP DATABASE %local_database_prefix%;"
+	echo STEP 4 of 6 :drop database %local_database_prefix%.
+	mysql -u %local_user% -e"DROP DATABASE %local_database_prefix%;"
 
-echo STEP 5 of 6 :create database %local_database_prefix%.
-mysql -u %local_user% -e"CREATE DATABASE %local_database_prefix%"
+	echo STEP 5 of 6 :create database %local_database_prefix%.
+	mysql -u %local_user% -e"CREATE DATABASE %local_database_prefix%"
 
-echo STEP 6 of 6 :dump new database
-mysql -u %local_user% %local_database_prefix% < %tableName%.sql
+	echo STEP 6 of 6 :dump new database
+	mysql -u %local_user% %local_database_prefix% < %tableName%.sql
 
-echo DONE!
+	echo DONE!
+) || (
+	echo Fail to dump remote database.
+)
 
 
